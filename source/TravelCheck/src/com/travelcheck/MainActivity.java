92efa6b619efc713157334f458f01c971eada7ed
Aditi <saveme925@gmail.com>
2015-01-10 09:27:22 -0800

    MainActivity -sending address with attachement , GPS enable popup

--- source/TravelCheck/src/com/travelcheck/MainActivity.java
+++ source/TravelCheck/src/com/travelcheck/MainActivity.java
@@ -5,21 +5,29 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.List;
+import java.util.Locale;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationManager;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.Settings;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity implements OnClickListener {
 	public Button CameraBtn;
@@ -31,18 +39,24 @@ public class MainActivity extends Activity implements OnClickListener {
 	boolean enabled;
 	Bitmap photo;
 	double latitude,longitude;
-
+	public TextView BottomTxt;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		extStorageDirectory = Environment.getExternalStorageDirectory()
 				.toString();
-		service = (LocationManager) getSystemService(LOCATION_SERVICE);
+		//service = (LocationManager) getSystemService(LOCATION_SERVICE);
+		 
+		//location = service.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+		//latitude=location.getLatitude();
+		//longitude=location.getLongitude();
 		enabled = service.isProviderEnabled(LocationManager.GPS_PROVIDER); 
-		location = service.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-		latitude=location.getLatitude();
-		longitude=location.getLongitude();
+		if(!enabled)
+		{
+			buildAlertMessageNoGps();
+		}
 		init();
 	}
 
@@ -50,6 +64,7 @@ public class MainActivity extends Activity implements OnClickListener {
 	public void init() {
 		CameraBtn = (Button) findViewById(R.id.CameraBtn);
 		CameraBtn.setOnClickListener(this);
+		BottomTxt=(TextView) findViewById(R.id.LocationTxt);
 
 	}
 
@@ -73,7 +88,7 @@ public class MainActivity extends Activity implements OnClickListener {
 	protected void onResume() {
 		// TODO Auto-generated method stub
 		super.onResume();
-
+		new GetAddressTask(getApplicationContext());
 	}
 
 	// store image in sd-card
@@ -155,54 +170,82 @@ public class MainActivity extends Activity implements OnClickListener {
 		alert.show();
 	}
 	
-/*	private static final String TAG = "LocationAddress";
+	 private class GetAddressTask extends
+     AsyncTask<Location, Void, String> {
+ Context mContext;
+ public GetAddressTask(Context context) {
+     super();
+     mContext = context;
+ }
 
-    public static void getAddressFromLocation(final double latitude, final double longitude,
-                                              final Context context, final Handler handler) {
-        Thread thread = new Thread() {
-            @Override
-            public void run() {
-                Geocoder geocoder = new Geocoder(context, Locale.getDefault());
-                String result = null;
-                try {
-                    List<Address> addressList = geocoder.getFromLocation(
-                            latitude, longitude, 1);
-                    if (addressList != null && addressList.size() > 0) {
-                        Address address = addressList.get(0);
-                        StringBuilder sb = new StringBuilder();
-                        for (int i = 0; i < address.getMaxAddressLineIndex(); i++) {
-                            sb.append(address.getAddressLine(i)).append("\n");
-                        }
-                        sb.append(address.getLocality()).append("\n");
-                        sb.append(address.getPostalCode()).append("\n");
-                        sb.append(address.getCountryName());
-                        result = sb.toString();
-                    }
-                } catch (IOException e) {
-                    Log.e(TAG, "Unable connect to Geocoder", e);
-                } finally {
-                    Message message = Message.obtain();
-                    message.setTarget(handler);
-                    if (result != null) {
-                        message.what = 1;
-                        Bundle bundle = new Bundle();
-                        result = "Latitude: " + latitude + " Longitude: " + longitude +
-                                "\n\nAddress:\n" + result;
-                        bundle.putString("address", result);
-                        message.setData(bundle);
-                    } else {
-                        message.what = 1;
-                        Bundle bundle = new Bundle();
-                        result = "Latitude: " + latitude + " Longitude: " + longitude +
-                                "\n Unable to get address for this lat-long.";
-                        bundle.putString("address", result);
-                        message.setData(bundle);
-                    }
-                    message.sendToTarget();
-                }
-            }
-        };
-        thread.start();
-    }
-}*/
+ /**
+  * Get a Geocoder instance, get the latitude and longitude
+  * look up the address, and return it
+  *
+  * @params params One or more Location objects
+  * @return A string containing the address of the current
+  * location, or an empty string if no address can be found,
+  * or an error message
+  */
+ @Override
+ protected String doInBackground(Location... params) {
+     Geocoder geocoder =
+             new Geocoder(mContext, Locale.getDefault());
+     // Get the current location from the input parameter list
+     Location loc = params[0];
+     // Create a list to contain the result address
+     List<Address> addresses = null;
+     try {
+         /*
+          * Return 1 address.
+          */
+         addresses = geocoder.getFromLocation(loc.getLatitude(),
+                 loc.getLongitude(), 1);
+     } catch (IOException e1) {
+     Log.e("LocationSampleActivity",
+             "IO Exception in getFromLocation()");
+     e1.printStackTrace();
+     return ("IO Exception trying to get address");
+     } catch (IllegalArgumentException e2) {
+     // Error message to post in the log
+     String errorString = "Illegal arguments " +
+             Double.toString(loc.getLatitude()) +
+             " , " +
+             Double.toString(loc.getLongitude()) +
+             " passed to address service";
+     Log.e("LocationSampleActivity", errorString);
+     e2.printStackTrace();
+     return errorString;
+     }
+     // If the reverse geocode returned an address
+     if (addresses != null && addresses.size() > 0) {
+         // Get the first address
+         Address address = addresses.get(0);
+         /*
+          * Format the first line of address (if available),
+          * city, and country name.
+          */
+         String addressText = String.format(
+                 "%s, %s, %s",
+                 // If there's a street address, add it
+                 address.getMaxAddressLineIndex() > 0 ?
+                         address.getAddressLine(0) : "",
+                 // Locality is usually a city
+                 address.getLocality(),
+                 // The country of the address
+                 address.getCountryName());
+         // Return the text
+         return addressText;
+     } else {
+         return "No address found";
+     }
+     
+ }
+ protected void onPostExecute(String address) {
+ 
+     //set Text Address in textview here
+	 BottomTxt.setText(address);
+ }
+
+}
 }
